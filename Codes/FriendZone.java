ea3010f1b5c9f600b74aaf35016fdf502b58c24a
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 11:34:22 -0500

    Create FriendZone.java

---
+++ Codes/FriendZone.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class FriendZone {
+   public static void main(String[] args) {
+     Scanner entrada= new Scanner (System.in);
+     String Nombre=entrada.next();
+     String Nombre2=entrada.next();
+     System.out.println(Nombre+" deja en la friend zone a "+Nombre2);
+   }
+}
