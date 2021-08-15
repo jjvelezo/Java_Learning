d92a1ebefecbe1d70d79723dbf5efa40336c90de
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:52:31 -0500

    Create Char.java

---
+++ Codes/Pros/Char.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+Random letra = new Random();
+char abc = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc);
+
+
+char abc2 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc2);
+
+
+char abc3 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc3);
+
+
+char abc4 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc4);
+
+
+char abc5 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc5);
+
+
+char abc6 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc6);
+
+
+char abc7 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc7);
+
+
+char abc8 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc8);
+
+
+char abc9 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc9);
+
+char abc10 = (char)(letra.nextInt(26) + 'a');
+System.out.println(abc10);        
+
+
+
+
+
+
+    }
+
+}
