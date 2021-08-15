319d22153e9c2e7a1170051676ef2d928b2accb7
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:13:23 -0500

    Create Helado.java

---
+++ Codes/Estructuras de Selección (Condicionales)/Helado.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Helado {
+   public static void main(String[] args) {
+    Scanner entrada= new Scanner (System.in);
+       
+       
+    double temp=entrada.nextDouble();
+    
+    if (temp>27)
+   
+   {
+       System.out.println("Comprar helado cerveza");
+   }
+   else;
+   
+
+   }
+} 
