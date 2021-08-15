984bdf5d9c7a7e5c69a681f1aa774c65bb90d954
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:41:50 -0500

    Create PorDebajoDelNumeroQueEscriva.java
    Para que imprima todos los números por debajo desde el numero que yo le escriba

---
+++ Codes/Pros/PorDebajoDelNumeroQueEscriva.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Multiplos5 {
+    public static void main(String args[]){
+        
+    
+    Scanner entrada= new Scanner (System.in);
+    
+    int multi=entrada.nextInt();
+    
+    {
+        for(int contador = 0; contador <= multi; contador++)
+        {
+            System.out.println(contador);
+        }
+    }
