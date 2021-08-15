dc48ceaa959fe070f0018e117b8b6809ecc429e8
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:23:05 -0500

    Create Infinito.java

---
+++ Codes/Estructuras de iteración II/Infinito.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Infinito {
+   public static void main(String[] args) {
+    Scanner entrada= new Scanner (System.in);
+    
+    while(true){
+        
+        
+    int numero=entrada.nextInt();
+  
+    if (numero == 0){
+        break;
+    }
+    
+        if (numero !=0){
+        
+
+        int multiplicacion = numero*3;
+        }
+        
+    
+        
+        System.out.println(numero*3);
+        }
+        
+
+   }
+}
