5a12fff195417510ac17209af248e4e41b86b221
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:31:22 -0500

    Create SumaMatrizDiagonal.java

---
+++ Codes/Arreglos/SumaMatrizDiagonal.java
@@ -0,0 +1,21 @@
+public class Array{
+    
+    public static int sumaDiagonal(int[][] args){
+        
+        int este=0;
+        
+        for(int i=0;i<args.length;i++){
+            
+            for(int j=0;j<args[i].length;j++){
+                
+                if(i==j){
+                    
+                    este += args[i][j];
+}
+   }
+}
+
+return este;
+        }
+        
+    }
