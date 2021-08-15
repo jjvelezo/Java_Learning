c92f13e8003bec0c986ceaf77bb8d3837139f896
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:26:40 -0500

    Create Overloading.java

---
+++ Codes/Objetos Revisión/Overloading.java
@@ -0,0 +1,31 @@
+public class Overloading {
+ 
+ 
+ public static void mostrar(int a){
+     
+     System.out.println("Se ha ingresado un entero");
+     
+ }
+ 
+ public static void mostrar(double b){
+     
+     System.out.println("Se ha ingresado un double");
+     
+ }
+ 
+ public static void mostrar(String c){
+     
+     System.out.println("Se ha ingresado un string");
+     
+ }
+ 
+ public static void mostrar(char d){
+     
+     System.out.println("Se ha ingresado un caracter");
+     
+ }
+ 
+    
+
+   
+}  
