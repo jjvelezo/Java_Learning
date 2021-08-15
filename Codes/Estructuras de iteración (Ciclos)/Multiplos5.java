a3c1dbff82ceba038574a3f3fc156751dca2eaaf
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:20:02 -0500

    Create Multiplos5.java

---
+++ Codes/Estructuras de iteración (Ciclos)/Multiplos5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Multiplos5 {
+    public static void main(String args[]){
+        
+    
+    Scanner entrada= new Scanner (System.in);
+    
+    int multi=entrada.nextInt();
+    
+    {
+        for(int contador = 5; contador <= multi; contador+=5)
+        {
+        
+          
+        System.out.println(contador);
+        }
+    }
+}
+}
