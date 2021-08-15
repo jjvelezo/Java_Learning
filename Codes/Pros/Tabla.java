e8ef96c98ce7600a48fe8eccfe7a995bbb7cd08a
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:51:47 -0500

    Create Tabla.java

---
+++ Codes/Pros/Tabla.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+   public static void main(String[] args) {
+
+Scanner entrada= new Scanner (System.in);
+
+
+System.out.println("---------------------");      
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+System.out.println("| | | | | | | | | | |");
+System.out.println("---------------------");
+
+
+
+
+   }
+}    
