29d701bbe6b07b566a37131e1cf4074f11bc810a
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 12:19:06 -0500

    Create Monos.java

---
+++ Codes/Estructuras de Selección (Condicionales)/Monos.java
@@ -0,0 +1,11 @@
+public class Monos {
+    public boolean problemaMonos(boolean sonrie_a, boolean sonrie_b)
+{
+    
+if((sonrie_b==false)&&(sonrie_a==false)||(sonrie_a==true)&&(sonrie_b==true))
+{return true;}
+else{return false;
+}
+    
+}
+}
