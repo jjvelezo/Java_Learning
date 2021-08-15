55c34b1dc299de062122a54f9cc4a313a513d2ef
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 11:35:03 -0500

    Create Mayores.java

---
+++ Codes/Mayores.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Mayores {
+   public static void main(String[] args) {
+    Scanner entrada= new Scanner (System.in);
+    int Numero1=entrada.nextInt();
+    int Numero2=entrada.nextInt();
+    System.out.println(Numero1 + Numero2);
+    System.out.println(Numero1 * Numero2);
+   }
+} 
