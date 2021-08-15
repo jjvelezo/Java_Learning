a989fe3f67e84a4865291615773ae0721109f084
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2021-08-15 11:33:19 -0500

    Create Asteroides.java

---
+++ Codes/Asteroides.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Asteroides {
+
+    public static void main(String[] args) {
+    Scanner entrada= new Scanner (System.in);
+    int NumeroAs=entrada.nextInt();
+    String Nombre=entrada.next();
+    System.out.println("Los "+NumeroAs+" asteroides "+Nombre+" caen del cielo");
+    
+    }
+}
